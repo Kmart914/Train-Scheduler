8c5d60f626e0ab1d0fa8cf8279e336366a949811
Kevin Martzolf <kmart914@gmail.com>
2017-08-04 15:30:12 -0700

    CSS done, and info sending to Firebase. Need to display it on screen and implement moment.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+var config = {
+   apiKey: "AIzaSyA06R83asalKiuxBasMaDFlwiFz_1k47gU",
+   authDomain: "test-project-78a2d.firebaseapp.com",
+   databaseURL: "https://test-project-78a2d.firebaseio.com",
+   projectId: "test-project-78a2d",
+   storageBucket: "test-project-78a2d.appspot.com",
+   messagingSenderId: "550930009757"
+ };
+ firebase.initializeApp(config);
+
+ var database = firebase.database();
+
+ var trainName = "";
+ var destination = "";
+ var firstTrain = "";
+ var frequency = "";
+
+ $("#addButton").on("click", function() {
+   console.log("sfg");
+
+   trainName = $('#inputName').val();
+   destination = $('#inputDest').val();
+   firstTrain = $('#inputTime').val();
+   frequency = $('#inputFreq').val();
+
+   database.ref().push({
+     trainName: trainName,
+     destination: destination,
+     firstTrain: firstTrain,
+     frequency: frequency
+   })
+
+ });
+
+ database.ref().on("child_added", function(childSnapshot){
+   var snap = childSnapshot.val();
+   console.log(snap);
+ });
